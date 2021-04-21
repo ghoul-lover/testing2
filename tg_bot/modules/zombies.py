fb143d2958707bf081d31465c1bb47b356236ae7
gagaoqphs <81736470+gagaoqphs@users.noreply.github.com>
2021-04-21 11:25:58 +0530

    Update zombies.py

--- tg_bot/modules/zombies.py
+++ tg_bot/modules/zombies.py
@@ -49,7 +49,7 @@ async def is_administrator(user_id: int, message):
 
 
 
-@telethn.on(events.NewMessage(pattern=f"^[!/]zombies ?(.*)"))
+@oko.on(events.NewMessage(pattern=f"^[!/]zombies ?(.*)"))
 async def zombies(event):
     """ For .zombies command, list all the zombies in a chat. """
 
